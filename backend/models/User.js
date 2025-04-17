ff218bcd43adbb5e3a310b670bc23e7a9a1cfb08
Shafin Kazi <kazishafinraza@gmail.com>
2025-04-18 01:20:30 +0530

    Included backend part for the application

---
+++ backend/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema(
+    {
+        fullName: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        profileImageUrl: { type: String, default: null },
+
+
+    },
+    { timestamps: true }
+)
+
+//hash password before saving user
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+
+});
+
+//cmpare pass
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("user", UserSchema);
+
