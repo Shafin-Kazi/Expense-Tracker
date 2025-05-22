0e6a8422fb007589747166885cb8d8158ff4d237
Shafin Kazi <kazishafinraza@gmail.com>
2025-05-23 00:40:57 +0530

    Dashboard Layout

--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -16,3 +16,14 @@ export const getInitials = (name) => {
 
     return initials.toUpperCase();
 };
+
+export const addThousandsSeparator = (num) => {
+
+    if (num == null || isNaN(num)) return "";
+
+    const [integerPart, fractionalPart] = num.toString().split(".");
+    const formattedIntegerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    return fractionalPart ? `${formattedIntegerPart}.${fractionalPart}` : formattedIntegerPart;
+}
+
